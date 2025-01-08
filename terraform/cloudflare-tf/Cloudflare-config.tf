78bdbff694b3684b4956c626e94eb7a4f2f6a175
Protocol789 <dj_protocol@hotmail.com>
2025-01-08 21:49:44 +0000

    cfg: Process updated

--- terraform/cloudflare-tf/Cloudflare-config.tf
+++ terraform/cloudflare-tf/Cloudflare-config.tf
@@ -1,18 +1,40 @@
 # Creates the CNAME record that routes http_app.${var.cloudflare_zone} to the tunnel.
+
+# 52abbc52-77c4-4b59-8f7e-7ec09b646b85
+# a2cb44dd-8d73-4761-8921-1b88838b0baa.cfargotunnel.com
+# Frontend Server
+
+data "cloudflare_zero_trust_tunnel_cloudflared" "http_tunnel" {
+  account_id = var.cloudflare_account_id
+  name = "Frontend Server"
+}
+
 resource "cloudflare_record" "http_app" {
   zone_id = var.cloudflare_zone_id
   name    = "http_app"
-  content   = "${cloudflare_zero_trust_tunnel_cloudflared.auto_tunnel.cname}"
+  content = "${data.cloudflare_zero_trust_tunnel_cloudflared.http_tunnel.id}.cfargotunnel.com"
   type    = "CNAME"
   proxied = true
+
 }
+
+# resource "cloudflare_dns_record" "http_app" {
+#   zone_id = var.cloudflare_zone_id
+#   name    = "http_app"
+#   # value   = cloudflare_zero_trust_tunnel_cloudflared.http_tunnel.tunnel_id
+#   type    = "CNAME"
+#   proxied = true
+#   ttl     = 3600
+# }
+
 
 # Creates an Access application to control who can connect.
 resource "cloudflare_zero_trust_access_application" "http_app" {
   zone_id          = var.cloudflare_zone_id
   name             = "Access application for http_app.${var.cloudflare_zone}"
   domain           = "http_app.${var.cloudflare_zone}"
-  session_duration = "1h"
+  type             = "self_hosted"
+  session_duration = "24h"
 }
 
 # Creates an Access policy for the application.

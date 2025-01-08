d03ce69bdb570b84092d12adf2511de516a6f615
Protocol789 <dj_protocol@hotmail.com>
2025-01-08 20:46:57 +0000

    traefik

--- docs/python/traefik/create_etcd_entry.py
+++ docs/python/traefik/create_etcd_entry.py
@@ -46,24 +46,40 @@ traefik.http.routers.{{ service_name }}.tls.domains.0.sans: '*.{{ base_domain }}
 traefik.http.services.{{ service_name }}.loadBalancer.servers.0.url: '{{ service_url }}'
 """
 
+def loop_keys(file):
+  print('hello')
+
 if __name__ == "__main__":
 
-  # source = template_yaml
-  source = template_json
+  source = template_yaml
+  # source = template_json
 
   if source == template_yaml: 
 
-    service_value = 'sabnzbd'  # You can change this to any other service name
+    service_value = 'riven'  # You can change this to any other service name
     base_domain = 'zorab.im'
     service_port = '8082'
-    service_url = 'http://frontend.server:8082'
+    service_url = 'http://frontend.server:3000'
 
     # Yaml file jinja
     jinja2_yml = traefik_conf(template_yaml,service_value, base_domain, service_port, service_url)
+    
+    # Load up yaml file into dict
     jinj2_cnv = yaml.safe_load(jinja2_yml)
 
+    # Replace . with / for flattened file
+    # jinj2_cnv.replace(".","/")
+    for get in jinj2_cnv.items():
+      key = str(get[0]).replace(".","/")
+      value = str(get[1])
+      print(key)
+      print(value)
+      put_kv(key,value)
+      get_kv(key,value)    
+
     # Unflatten file
-    unflat_doc = unflatten_doc(jinj2_cnv)
+    # unflat_doc = unflatten_doc(jinj2_cnv)
+
     # flat_file = flatten_file(jinj2_cnv)
     # jinj2_cnv.replace(".","/")
     # flatten_File = flatten(jinj2_cnv,".")
